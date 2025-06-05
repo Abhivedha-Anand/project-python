7457da226b676a7d4c07eb1fc41659ee52385c86
Naveenjr620 <naveenjr620@gmail.com>
2025-06-05 15:47:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template, session, url_for
+from flask import Flask, request, redirect, render_template, session, url_for, jsonify
 import pandas as pd
 import os
 from datetime import datetime
